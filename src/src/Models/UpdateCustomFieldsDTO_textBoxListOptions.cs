065d39ff38aa41d2db35e329ee8b3b3d955f4e95
Jake Soenneker <jake@soenneker.com>
2026-02-20 17:37:36 +0000

    Automated update

--- src/src/Models/UpdateCustomFieldsDTO_textBoxListOptions.cs
+++ src/src/Models/UpdateCustomFieldsDTO_textBoxListOptions.cs
@@ -9,43 +9,43 @@ namespace Soenneker.HighLevel.OpenApiClient.Models
 {
     [global::System.CodeDom.Compiler.GeneratedCode("Kiota", "1.0.0")]
     #pragma warning disable CS1591
-    public partial class ProductSEODto : IAdditionalDataHolder, IParsable
+    public partial class UpdateCustomFieldsDTO_textBoxListOptions : IAdditionalDataHolder, IParsable
     #pragma warning restore CS1591
     {
         /// <summary>Stores additional data not described in the OpenAPI description found when deserializing. Can be used for serialization as well.</summary>
         public IDictionary<string, object> AdditionalData { get; set; }
-        /// <summary>SEO description</summary>
+        /// <summary>The label property</summary>
 #if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
 #nullable enable
-        public string? Description { get; set; }
+        public string? Label { get; set; }
 #nullable restore
 #else
-        public string Description { get; set; }
+        public string Label { get; set; }
 #endif
-        /// <summary>SEO title</summary>
+        /// <summary>The prefillValue property</summary>
 #if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
 #nullable enable
-        public string? Title { get; set; }
+        public string? PrefillValue { get; set; }
 #nullable restore
 #else
-        public string Title { get; set; }
+        public string PrefillValue { get; set; }
 #endif
         /// <summary>
-        /// Instantiates a new <see cref="global::Soenneker.HighLevel.OpenApiClient.Models.ProductSEODto"/> and sets the default values.
+        /// Instantiates a new <see cref="global::Soenneker.HighLevel.OpenApiClient.Models.UpdateCustomFieldsDTO_textBoxListOptions"/> and sets the default values.
         /// </summary>
-        public ProductSEODto()
+        public UpdateCustomFieldsDTO_textBoxListOptions()
         {
             AdditionalData = new Dictionary<string, object>();
         }
         /// <summary>
         /// Creates a new instance of the appropriate class based on discriminator value
         /// </summary>
-        /// <returns>A <see cref="global::Soenneker.HighLevel.OpenApiClient.Models.ProductSEODto"/></returns>
+        /// <returns>A <see cref="global::Soenneker.HighLevel.OpenApiClient.Models.UpdateCustomFieldsDTO_textBoxListOptions"/></returns>
         /// <param name="parseNode">The parse node to use to read the discriminator value and create the object</param>
-        public static global::Soenneker.HighLevel.OpenApiClient.Models.ProductSEODto CreateFromDiscriminatorValue(IParseNode parseNode)
+        public static global::Soenneker.HighLevel.OpenApiClient.Models.UpdateCustomFieldsDTO_textBoxListOptions CreateFromDiscriminatorValue(IParseNode parseNode)
         {
             if(ReferenceEquals(parseNode, null)) throw new ArgumentNullException(nameof(parseNode));
-            return new global::Soenneker.HighLevel.OpenApiClient.Models.ProductSEODto();
+            return new global::Soenneker.HighLevel.OpenApiClient.Models.UpdateCustomFieldsDTO_textBoxListOptions();
         }
         /// <summary>
         /// The deserialization information for the current model
@@ -55,8 +55,8 @@ namespace Soenneker.HighLevel.OpenApiClient.Models
         {
             return new Dictionary<string, Action<IParseNode>>
             {
-                { "description", n => { Description = n.GetStringValue(); } },
-                { "title", n => { Title = n.GetStringValue(); } },
+                { "label", n => { Label = n.GetStringValue(); } },
+                { "prefillValue", n => { PrefillValue = n.GetStringValue(); } },
             };
         }
         /// <summary>
@@ -66,8 +66,8 @@ namespace Soenneker.HighLevel.OpenApiClient.Models
         public virtual void Serialize(ISerializationWriter writer)
         {
             if(ReferenceEquals(writer, null)) throw new ArgumentNullException(nameof(writer));
-            writer.WriteStringValue("description", Description);
-            writer.WriteStringValue("title", Title);
+            writer.WriteStringValue("label", Label);
+            writer.WriteStringValue("prefillValue", PrefillValue);
             writer.WriteAdditionalData(AdditionalData);
         }
     }
