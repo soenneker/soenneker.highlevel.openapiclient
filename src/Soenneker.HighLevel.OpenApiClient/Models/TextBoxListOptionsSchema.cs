849af45f79347df20774b56d4c06706a6f7e97b7
Jake Soenneker <jake@soeneker.com>
2026-04-11 17:31:28 +0000

    Automated update

--- src/Soenneker.HighLevel.OpenApiClient/Models/TextBoxListOptionsSchema.cs
+++ src/Soenneker.HighLevel.OpenApiClient/Models/TextBoxListOptionsSchema.cs
@@ -9,7 +9,7 @@ namespace Soenneker.HighLevel.OpenApiClient.Models
 {
     [global::System.CodeDom.Compiler.GeneratedCode("Kiota", "1.0.0")]
     #pragma warning disable CS1591
-    public partial class CreateCustomFieldsDTO_textBoxListOptions : IAdditionalDataHolder, IParsable
+    public partial class TextBoxListOptionsSchema : IAdditionalDataHolder, IParsable
     #pragma warning restore CS1591
     {
         /// <summary>Stores additional data not described in the OpenAPI description found when deserializing. Can be used for serialization as well.</summary>
@@ -31,21 +31,21 @@ namespace Soenneker.HighLevel.OpenApiClient.Models
         public string PrefillValue { get; set; }
 #endif
         /// <summary>
-        /// Instantiates a new <see cref="global::Soenneker.HighLevel.OpenApiClient.Models.CreateCustomFieldsDTO_textBoxListOptions"/> and sets the default values.
+        /// Instantiates a new <see cref="global::Soenneker.HighLevel.OpenApiClient.Models.TextBoxListOptionsSchema"/> and sets the default values.
         /// </summary>
-        public CreateCustomFieldsDTO_textBoxListOptions()
+        public TextBoxListOptionsSchema()
         {
             AdditionalData = new Dictionary<string, object>();
         }
         /// <summary>
         /// Creates a new instance of the appropriate class based on discriminator value
         /// </summary>
-        /// <returns>A <see cref="global::Soenneker.HighLevel.OpenApiClient.Models.CreateCustomFieldsDTO_textBoxListOptions"/></returns>
+        /// <returns>A <see cref="global::Soenneker.HighLevel.OpenApiClient.Models.TextBoxListOptionsSchema"/></returns>
         /// <param name="parseNode">The parse node to use to read the discriminator value and create the object</param>
-        public static global::Soenneker.HighLevel.OpenApiClient.Models.CreateCustomFieldsDTO_textBoxListOptions CreateFromDiscriminatorValue(IParseNode parseNode)
+        public static global::Soenneker.HighLevel.OpenApiClient.Models.TextBoxListOptionsSchema CreateFromDiscriminatorValue(IParseNode parseNode)
         {
             if(ReferenceEquals(parseNode, null)) throw new ArgumentNullException(nameof(parseNode));
-            return new global::Soenneker.HighLevel.OpenApiClient.Models.CreateCustomFieldsDTO_textBoxListOptions();
+            return new global::Soenneker.HighLevel.OpenApiClient.Models.TextBoxListOptionsSchema();
         }
         /// <summary>
         /// The deserialization information for the current model
